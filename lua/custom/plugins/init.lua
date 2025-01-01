50d7863e1877cb05511db0048e6300563512c188
Venoflux <erolsarpk@gmail.com>
2025-01-01 11:46:18 +0300

    Initial customizations

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,8 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.bo.softtabstop = 2
 return {}
